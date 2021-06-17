445d0196a073642a24b01b4011292d37bb9db2e8
Cloud User <centos@ip-172-31-3-163.eu-west-2.compute.internal>
2021-06-17 11:12:20 +0000

    A working solution to Ex6

--- Ex6/top.v
+++ Ex6/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #6 - RGB Colour Converter
-// Student Name:
-// Date: 
+// Student Name:Amelia Carse
+// Date: 17/06/21
 //
 //
 //  Description: In this exercise, you need to design a memory with 8 entries, 
@@ -14,4 +14,22 @@
 //           rgb [23:0]
 //
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module RGB_converter(
+    //Todo: add ports 
+	input clk,
+        input [2:0] colour,
+	input enable,
+	output [23:0] rgb
+    );
+//IP core
+	blk_mem_gen_0 top (
+	  .addra(colour), //colour input [2:0]
+	  .clka(clk),         
+	  .dina(24'b0), //input = 3x8bit   
+	  .douta(rgb), // output rgb[23:0]
+	  .ena(enable),      
+	  .wea(1'b0)  
+	);
+endmodule
--- Ex6/top_tb.v
+++ Ex6/top_tb.v
@@ -1,9 +1,82 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #6 - RGB Colour Converter
-// Student Name:
-// Date: 
+// Student Name: Amelia Carse
+// Date: 17/06/21
 //
 // Description: A testbench module to test Ex6 - RGB Colour Converter
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module top_tb(
+    );
+    
+//Todo: Parameters
+parameter CLK_PERIOD = 10;
+
+//Todo: Regitsers and wires
+    reg clk;
+    reg err;
+    reg [2:0] colour;
+    wire [23:0] rgb;
+    reg enable;
+
+//Todo: Clock generation
+  initial begin
+       clk = 1'b0;
+       forever
+         #(CLK_PERIOD/2) clk=~clk;
+     end
+
+//Todo: User logic
+initial begin
+       enable=0;
+       err=0;
+       colour = 3'b000;
+       #(5*CLK_PERIOD);
+       
+	if(rgb != 24'h0) begin
+		   $display("***TEST FAILED! RGB must be 0 when enable is 0***");
+		   err=1;
+	end
+
+ end
+
+
+initial begin
+enable = 1;
+#CLK_PERIOD;
+       forever begin
+         #CLK_PERIOD;
+
+	err<=(((colour[0])==(&rgb[7:0]))&((colour[1])==(&rgb[15:8]))&((colour[2])==(&rgb[23:16])))?0: 1;
+		if(err == 1)
+          		begin 
+	    		$display("***TEST FAILED! RGB and colour do not match");
+            
+		end
+	colour = colour+ 3'b001; 
+            
+	end
+end
+	
+
+	    
+//Todo: Finish test, check for success
+initial begin
+        #500
+        if (err==0)
+          $display("***TEST PASSED! :) ***");
+        $finish;
+      end
+
+
+//Todo: Instantiate counter module
+RGB_converter top(
+     .enable (enable),
+     .clk (clk),
+     .rgb (rgb),
+     .colour (colour)
+     );
+ 
+endmodule 
